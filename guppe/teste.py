7cce8b40ad221051ba51088332e66c60fc094fc7
LuciLua <luci.lua81@gmail.com>
2023-02-07 20:58:41 -0300

    🐍 configurando o ambiente

---
+++ guppe/teste.py
@@ -0,0 +1,22 @@
+# Run: ctrl + \
+# Ambiente Virtual preparado
+# Ferramentas que permitem criar ambientes isolados para evitar conflitos entre as bibliotecas
+# virtualenv
+# virtualenvwrapper
+
+# Para criar ambiente virtual com versoes mais antigas do python:
+# mkvirtualenv nomeDoProjeto -p python3.7
+# ! preciso ter a versao desejada instalada !
+
+
+URL = "https://lucilua.com.br"
+print(f'My website: {URL}')
+
+
+
+
+
+
+
+
+
