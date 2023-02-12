a7465a7ae438e98c3ea3039284688ab5a701075a
LuciLua <luci.lua81@gmail.com>
2023-02-12 20:01:33 -0300

    :star2: [11] - Dir e Help

---
+++ sessao 3/dirHelp.py
@@ -0,0 +1,31 @@
+"""
+Utilitatios python para auxiliar no programacao
+
+Dir => Apresenta todos os atributos e funçoes/métodos
+disponíveis para determinado tipo de dado ou variavel
+
+Help => Apresenta a documentação/como utilizar
+os atributos/propriedades e funções/métodos disponíveis
+para determinado tipo de dado ou variável.
+
+dir("tipo de dadod")
+help("descobrir o que tal coisa faz".lower)
+
+upper() => converte tudo para maiusculo
+lower() => converte tudo para minusculo
+title() => somente a primeira de todas as palavras para maiusculo
+
+__add__ => para somar
+ex.: num.__add__(3)
+soma num com (3)
+"""
+
+print(dir('ola'))
+
+num = 4
+
+# print(help(num))
+
+print(dir(num))
+
+
