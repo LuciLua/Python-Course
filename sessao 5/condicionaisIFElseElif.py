35686c135d25aea4678b3c4124b366ff32fb4bed
LuciLua <luci.lua81@gmail.com>
2023-02-16 18:53:11 -0300

    :star2: [22] - if, elif, else

---
+++ sessao 5/condicionaisIFElseElif.py
@@ -0,0 +1,20 @@
+"""
+Estruturas condicionais:
+
+[ ! ] - if
+[ ! ] - elif
+[ ! ] - else
+
+=> Novo bloco no python: : + 4 espaços depois
+"""
+
+idade = 22
+
+if idade < 18:
+    print('Menor de idade')
+elif idade == 22:
+    print("tem 22 anos")
+elif idade > 30:
+    print("idade maior que 30")
+else:
+    print("maior de idade menor que 30")
